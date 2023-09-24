e7e54511f7f69e79f3b0d1c63036263a3d8fcd95
Mohammad <mohammadtohidi.mtn@gmail.com>
2023-09-24 20:42:12 +0330

    fix bugs

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
 
     # Local apps
     'home.apps.HomeConfig',
-    'accounts.apps.AccountsConfig',
+    'user.apps.UserConfig',
     'orders.apps.OrdersConfig',
 
     # Third-party apps
@@ -84,14 +84,11 @@ WSGI_APPLICATION = 'A.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'jack',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -153,7 +150,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'accounts.User'
+AUTH_USER_MODEL = 'user.User'
 
 
 # ARVAN CLOUD STORAGE
