d7e34ad9ee08b8212f4a63e25d3d511109ff1f63
Mohammad <mohammadtohidi.mtn@gmail.com>
2023-10-12 15:28:12 +0330

    users app cheeck using black

--- user/management/commands/remove_expired_otps.py
+++ user/management/commands/remove_expired_otps.py
@@ -5,9 +5,11 @@ import pytz
 
 
 class Command(BaseCommand):
-	help = 'remove all expired otp codes'
+    help = "remove all expired otp codes"
 
-	def handle(self, *args, **options):
-		expired_time = datetime.now(tz=pytz.timezone('Asia/Tehran')) - timedelta(minutes=2)
-		OtpCode.objects.filter(created__lt=expired_time).delete()
-		self.stdout.write('all expired otp codes removed.')
+    def handle(self, *args, **options):
+        expired_time = datetime.now(tz=pytz.timezone("Asia/Tehran")) - timedelta(
+            minutes=2
+        )
+        OtpCode.objects.filter(created__lt=expired_time).delete()
+        self.stdout.write("all expired otp codes removed.")
